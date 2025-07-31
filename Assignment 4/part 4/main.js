1a557484b70dcf01432a4a08ed8a7879a2fd229c
Ved-ux-cr <ved.patel9@dcmail.ca>
2025-07-30 20:16:46 -0400

    update js

--- Assignment 4/part 4/main.js
+++ Assignment 4/part 4/main.js
@@ -185,6 +185,17 @@ function loop() {
   ctx.fillStyle = 'rgba(0, 0, 0, 0.25)';
   ctx.fillRect(0, 0, width, height);
 
+  for (const ball of balls) {
+    if (ball.exists) {
+      ball.draw();
+      ball.update();
+      ball.collisionDetect();
+    }
+  }
+
+  evilBall.draw();
+  evilBall.checkBounds();
+  evilBall.collisionDetect();
 
   requestAnimationFrame(loop);
 }
